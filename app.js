0539306903bb678783ad42a3aaa9d97789e05101
Kiran <kirankumardn74@gmail.com>
2024-04-30 20:16:23 +0530

    crud operation

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+
+//middleware
+
+app.use(express.json());
+
+//routes
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+//app.get('/api/v1/tasks')  -get all the tasks
+//app.post("/api/v1/tasks") -create a new task
+//app.get("/api/v1/tasks/:id") -get single task
+//app.patch("/api/v1/tasks/:id") -update task
+//app.delete("/api/v1/tasks/:id") -delete task
+
+const PORT = 3000;
+
+app.listen(PORT, console.log(`server is listening on port ${PORT}...`));
